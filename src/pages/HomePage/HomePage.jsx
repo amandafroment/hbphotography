fcbd6ae1000fd2220dd1798ed6617f0e5384e980
Amanda Froment <amandafroment@amandas-mbp.lan>
2023-03-06 16:32:11 -0500

    services skeleton completed

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,6 +5,10 @@ import introductionmeimage from "../../images/introduction-me.jpeg";
 import honey1 from "../../images/honey1.jpeg";
 import honey2 from "../../images/honey2.jpeg";
 import honey3 from "../../images/honey3.jpeg";
+import honey4 from "../../images/honey4.jpeg";
+import honey5 from "../../images/honey5.jpeg";
+import honey6 from "../../images/honey6.jpeg";
+import honey7 from "../../images/honey7.jpeg";
 import { Link } from "react-router-dom";
 
 export default function HomePage() {
@@ -27,14 +31,14 @@ export default function HomePage() {
           <div className="Introduction-Inner paragraph">
             <h3>Hi There!</h3>
             <p className="introduction-paragraph">
-              I'm Amanda (She/Her), a photographer based in Hamilton, Ontario
-              and serving the surrounding areas. The sweet girl that you will
-              see quite a few pictures of throughout my website is my dog Honey,
-              a Husky Mix that I adopted in 2018! We're both adventurous,
-              welcoming and full of spirit, and love meeting new people. With
-              any photograph I take, I strive to bring out the love and joy that
-              a family and their furry friends have for each other - I would be
-              honoured to capture that for you.
+              I'm Amanda, the photographer behind Honey Bee Photography! I'm
+              based in Hamilton, Ontario and serving the surrounding areas. The
+              sweet girl that you will see quite a few pictures of throughout my
+              website is my dog Honey, a Husky Mix that I adopted in 2018! We're
+              both adventurous, welcoming and full of spirit, and love meeting
+              new people. With any photograph I take, I strive to bring out the
+              love and joy that a family and their furry friends have for each
+              other - I would be honoured to capture that for you.
             </p>
           </div>
           <div className="Introduction-Inner">
@@ -52,19 +56,27 @@ export default function HomePage() {
         <div className="ServicesIntroductionContainer">
           <div className="homepage-images-container">
             <img src={honey1} className="homepage-images"></img>
+            <div className="inner-image-container">
+              <img src={honey4} className="inner-image"></img>
+              <img src={honey5} className="inner-image"></img>
+            </div>
             <img src={honey2} className="homepage-images"></img>
+            <div className="inner-image-container">
+              <img src={honey6} className="inner-image"></img>
+              <img src={honey7} className="inner-image"></img>
+            </div>
             <img src={honey3} className="homepage-images"></img>
           </div>
           <div className="homepage-services-description">
             <p>
-              DO YOU LIKE WHAT YOU SEE?{" "}
+              DO YOU LIKE WHAT YOU SEE?<br></br>
               <Link to="/pricing" className="link-to-pages">
                 LEARN MORE ABOUT MY SERVICES!
               </Link>
             </p>
             <p className="or">OR</p>
             <p>
-              DO YOU HAVE ANY QUESTIONS?{" "}
+              DO YOU HAVE ANY QUESTIONS?<br></br>
               <Link to="/contactme" className="link-to-pages">
                 SEND ME AN EMAIL TO CONNECT!
               </Link>
--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,11 +1,49 @@
 import React from "react";
 import NavBar from "../../components/NavBarGeneral/NavBarGeneral";
+import "./Pricing.css";
 
 export default function Pricing() {
   return (
     <>
       <NavBar />
-      <div>Pricing</div>
+      <div className="ServicesContainer">
+        <div className="individual-service">
+          <div className="service-description">
+            <h2>Title w/ pricing</h2>
+            <p>Info paragraph</p>
+            <p>Details/What's Included</p>
+            <p>Promotions: one for adoption within 6 months, one for seniors</p>
+            <p>Booking Information (deposit, contract, etc.)</p>
+          </div>
+          <div className="service-image-container">
+            <img></img>
+            <p>Image container</p>
+          </div>
+        </div>
+
+        <div className="individual-service">
+          <div className="service-image-container">
+            <img></img>
+            <p>Image Container</p>
+          </div>
+          <div className="service-description">
+            <h2>Title w/ pricing</h2>
+            <p>Info paragraph</p>
+            <p>Details/What's Included</p>
+            <p>Promotions: one for adoption within 6 months, one for seniors</p>
+            <p>Booking Information (deposit, contract, etc.)</p>
+          </div>
+        </div>
+
+        <div>
+          <h2>Other types of shoots</h2>
+          <p>
+            Don't have a pet but like my style of photography? Send me a message
+            and let's chat - I would be more than happy to capture the life
+            moments you need!
+          </p>
+        </div>
+      </div>
     </>
   );
 }
