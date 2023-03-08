543c2880457b38e591d3192ba4627efdb34cf8c4
Amanda Froment <amandafroment@amandas-mbp.lan>
2023-03-08 09:41:16 -0500

    started styling contact form

--- src/pages/ContactMePage/ContactMePage.jsx
+++ src/pages/ContactMePage/ContactMePage.jsx
@@ -9,10 +9,21 @@ export default function ContactMePage() {
       <div className="ContactMePage">
         <div className="contactme-header-images">Images</div>
         <div className="contactme-container">
-          <h2>
-            Like what you've seen? Let's connect, send me a message below.
+          <h2 className="contactme-title">
+            LIKE WHAT YOU'VE SEEN? SEND ME A MESSAGE BELOW TO CONNECT!
           </h2>
-          <p>Put a form here</p>
+          <form>
+            <label>Your Name: </label>
+            <input type="text"></input>
+            <label>Email Address:</label>
+            <input type="email"></input>
+            <label>Phone Number:</label>
+            <input type="text"></input>
+            <label>
+              What type of session are you looking for/any other details?
+            </label>
+            <input type="text" className="contactme-details" max="500"></input>
+          </form>
         </div>
       </div>
     </>
